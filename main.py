6e4a6125e2563a539d6ce8dbe9d115b43fe64ea5
asmku259c2s22903 <144457418+asmku259c2s22903@users.noreply.github.com>
2023-09-14 18:43:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-def factorial(x):
-  if x == 1:
-    return 1
-  else:
-    return (x * factorial(x - 1))
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        else:
+            return False
 
-num = int(input("enter a value: "))
-result = factorial(num)
-print("The factorial of", num, "is", result)
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        return f"Account Balance for {self.__account_holder_name}: ${self.__account_balance:.2f}"
+if __name__ == "__main__":
+    account = BankAccount("123456", "John Doe", 1000.0)
+    if account.deposit(500.0):
+        print("Deposit successful!")
+    else:
+        print("Invalid deposit amount.")
+    if account.withdraw(200.0):
+        print("Withdrawal successful!")
+    else:
+        print("Insufficient balance or invalid withdrawal amount.")
+    print(account.display_balance())
